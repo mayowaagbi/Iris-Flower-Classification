0608f1f40730f1d5443d754bee01f75e713134df
mayowaagbi <agbimayowa3@gmail.com>
2025-01-03 17:29:52 +0100

    added api

---
+++ iris classification/main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI
+import joblib
+import numpy as np
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
+
+origins = [
+    # "http://localhost:3000",
+    "http://127.0.0.1:8000",
+]
+
+
+# Load the trained model
+model = joblib.load("iris_knn_model.pkl")
+
+# Create the FastAPI app
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+# Define the input structure
+class Features(BaseModel):
+    sepal_length: float
+    sepal_width: float
+    petal_length: float
+    petal_width: float
+
+
+# Create an endpoint for prediction
+@app.post("/predict/")
+def predict(features: Features):
+    try:
+        feature_array = np.array(
+            [
+                [
+                    features.sepal_length,
+                    features.sepal_width,
+                    features.petal_length,
+                    features.petal_width,
+                ]
+            ]
+        )
+        prediction = model.predict(feature_array)
+        return {"prediction": prediction[0]}
+    except Exception as e:
+        return {"error": str(e)}
